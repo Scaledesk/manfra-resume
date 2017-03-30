8425cae2d4772660c5cff8970d77f3907a04ba84
garima <garima8jun@gmail.com>
2017-03-30 13:11:18 +0530

    testimonial section

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@
 
 $(document).ready(function(){
     $('.testimonials-list').slick({
-        dots: true,
-        autoplay: true,
+        dots: false,
+        autoplay: false,
         prevArrow: '<span class="fa-stack testimonial-left-handle fa-lg"><i class="fa fa-square-o fa-stack-2x"></i><i class="fa fa-caret-left fa-stack-1x"></i></span>',
         nextArrow: '<span class="fa-stack testimonial-right-handle fa-lg"><i class="fa fa-square-o fa-stack-2x"></i><i class="fa fa-caret-right fa-stack-1x"></i></span>',
     });
