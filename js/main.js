d0f1869b45911e0e644abc121315c00cea1808fd
garima <garima8jun@gmail.com>
2017-04-08 11:01:36 +0530

    header added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,13 @@ $(document).ready(function(){
 });
 */
 
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".navbar-default").addClass("activescroll");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $(".navbar-default").removeClass("activescroll");
+        }
+    });
+});
